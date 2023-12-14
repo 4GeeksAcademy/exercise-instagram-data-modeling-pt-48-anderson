4729ed38fdb5542d838b799f1db1349fe9af7b96
Anderson Gonzalez <141663667+AndyGH86@users.noreply.github.com>
2023-12-14 09:25:03 +0000

    modelo hecho

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
@@ -27,6 +27,51 @@ class Address(Base):
 
     def to_dict(self):
         return {}
+    
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(30), nullable=False)
+    last_name = Column(String(30), nullable=False)
+    user_name = Column(String(50), nullable=False, unique=True)
+    email = Column(String(50), nullable=False, unique=True)
+    password = Column(String(20), nullable=False)
+
+
+class Post(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum, nullable=False)
+    url = Column(String, nullable=False)
+    post_id = Column(Integer, ForeignKey('posts.id'))
+
+
+class Comment(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(300), nullable=False)
+    author_id = Column(Integer, ForeignKey('users.id'))
+    post_id = Column(Integer, ForeignKey('posts.id'))
+
+
+class Follower(Base):
+    __tablename__ = 'followers'
+    id = Column(Integer, primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    user_from_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+
+
+
+
+
+
 
 ## Draw from SQLAlchemy base
 try:
